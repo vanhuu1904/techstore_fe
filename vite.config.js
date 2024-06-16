26f77b5293800c45d6cff5e848476c9593267bb0
Van Huu <huynam569@gmail.com>
2024-06-16 22:08:45 +0700

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://techstore-be.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, "/api"),
       },
